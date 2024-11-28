64d109e03510316b49075678a3feb4cd015e39e1
Adnan <adnancpanakkad@gmail.com>
2024-11-28 09:02:42 +0530

    (confg):git configuring

---
+++ android/app/src/main/kotlin/com/example/ayur_care/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.ayur_care
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
---
+++ lib/screens/splash_screen.dart
@@ -0,0 +1,39 @@
+import 'package:ayur_care/screens/login_screen.dart';
+import 'package:flutter/material.dart';
+
+class SplashScreen extends StatefulWidget {
+  const SplashScreen({super.key});
+
+  @override
+  State<SplashScreen> createState() => _SplashScreenState();
+}
+
+class _SplashScreenState extends State<SplashScreen> {
+  @override
+  void initState() {
+    super.initState();
+    _navigateToHome();
+  }
+
+  void _navigateToHome() async {
+    await Future.delayed(const Duration(seconds: 3)); // Splash duration
+    Navigator.pushReplacement(
+      context,
+      MaterialPageRoute(builder: (context) => LoginScreen()),
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Container(
+        decoration: const BoxDecoration(
+          image: DecorationImage(
+            image: AssetImage('assets/iPhone 13 & 14 - 1.png'),
+            fit: BoxFit.cover,
+          ),
+        ),
+      ),
+    );
+  }
+}
